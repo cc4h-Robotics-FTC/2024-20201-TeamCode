195e6e86ce6e3f3aa64e62760314cf0675a7f8bd
Joshua Focht <joshua.focht@outlook.com>
2024-12-14 15:49:01 -0500

    Replace splineTo with strafeTo and strafeToSplineHeading Added dumping both samples and pushing one into netzone
    Co-authored-by: Micah Hall <3DThangs@users.noreply.github.com>

--- MeepMeepTesting/src/main/java/io/github/cc4h_robotics_ftc/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/io/github/cc4h_robotics_ftc/meepmeeptesting/MeepMeepTesting.java
@@ -15,18 +15,22 @@ public class MeepMeepTesting {
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
                 .build();
 
-        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(-25.0, -62.0, Math.toRadians(90)))
-            .splineTo(new Vector2d(0.0, -34.0), Math.toRadians(90))
-            .waitSeconds(1.0) // TODO: Hang Specimen
-            .splineTo(new Vector2d(0.0, -54.0), Math.toRadians(90))
-            .splineTo(new Vector2d(-48.0, -40.0), Math.toRadians(90))
+        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(-32.0, -62.0, Math.toRadians(90.0)))
+            .strafeTo(new Vector2d(0.0, -52.0)) // Get into position
+            .strafeTo(new Vector2d(0.0, -34.0)) // Ram specimin into bar
+            .strafeToSplineHeading(new Vector2d(-48.0, -40.0), Math.toRadians(90.0)) // Line up with first sample
             .waitSeconds(1.0) // TODO: Pick Up Sample
-            .splineTo(new Vector2d(-52.0, -52.0), Math.toRadians(45))
+            .strafeToSplineHeading(new Vector2d(-52.0, -52.0), Math.toRadians(45.0)) // Line up with basket
             .waitSeconds(1.0) // TODO: Dump Sample
-            .turnTo(Math.toRadians(90))
-            .strafeTo(new Vector2d(18.0, -60.0))
-            .waitSeconds(12.0)
-            .strafeTo(new Vector2d(36.0, -60.0))
+            .strafeToSplineHeading(new Vector2d(-58.5, -40.0), Math.toRadians(90.0))
+            .waitSeconds(1.0) // TODO: Pick up second sample
+            .strafeToSplineHeading(new Vector2d(-52.0, -52.0), Math.toRadians(45.0))
+            .waitSeconds(1.0) // TODO: Dump second sample
+            .strafeToSplineHeading(new Vector2d(-52.0, -10.0), Math.toRadians(90.0))
+            .strafeTo(new Vector2d(-61.0, -10.0))
+            .strafeTo(new Vector2d(-61.0, -52.0))
+            .waitSeconds(8.0)
+            .strafeTo(new Vector2d(36.0, -60.0)) // Parking
             .build());
 
         meepMeep.setBackground(MeepMeep.Background.FIELD_INTO_THE_DEEP_JUICE_DARK)
