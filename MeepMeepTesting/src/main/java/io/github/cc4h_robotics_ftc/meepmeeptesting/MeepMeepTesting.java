ba490e64f2b09b4d5e3798876a0a30f14f16c71c
joshua <joshua.focht@outlook.com>
2024-12-14 10:37:46 -0500

    switch to java for auto

---
+++ MeepMeepTesting/src/main/java/io/github/cc4h_robotics_ftc/meepmeeptesting/MeepMeepTesting.java
@@ -0,0 +1,38 @@
+package io.github.cc4h_robotics_ftc.meepmeeptesting;
+
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.noahbres.meepmeep.MeepMeep;
+import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
+import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
+
+public class MeepMeepTesting {
+    public static void main(String[] args) {
+        MeepMeep meepMeep = new MeepMeep(600);
+
+        RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
+                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                .build();
+
+        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(-25.0, -62.0, Math.toRadians(90)))
+            .splineTo(new Vector2d(0.0, -34.0), Math.toRadians(90))
+            .waitSeconds(1.0) // TODO: Hang Specimen
+            .splineTo(new Vector2d(0.0, -54.0), Math.toRadians(90))
+            .splineTo(new Vector2d(-48.0, -40.0), Math.toRadians(90))
+            .waitSeconds(1.0) // TODO: Pick Up Sample
+            .splineTo(new Vector2d(-52.0, -52.0), Math.toRadians(45))
+            .waitSeconds(1.0) // TODO: Dump Sample
+            .turnTo(Math.toRadians(90))
+            .strafeTo(new Vector2d(18.0, -60.0))
+            .waitSeconds(12.0)
+            .strafeTo(new Vector2d(36.0, -60.0))
+            .build());
+
+        meepMeep.setBackground(MeepMeep.Background.FIELD_INTO_THE_DEEP_JUICE_DARK)
+                .setDarkMode(true)
+                .setBackgroundAlpha(0.95f)
+                .addEntity(myBot)
+                .start();
+    }
+}
