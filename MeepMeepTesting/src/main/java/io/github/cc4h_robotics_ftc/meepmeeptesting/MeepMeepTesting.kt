d29e32e6e67da26f55c1c96b23b7aad109038571
Micah Hall <hallmicah10@gmail.com>
2024-12-14 07:42:06 -0500

    Fix errors of teleportation and invertation. Also made the autonomous faster.

--- MeepMeepTesting/src/main/java/io/github/cc4h_robotics_ftc/meepmeeptesting/MeepMeepTesting.kt
+++ MeepMeepTesting/src/main/java/io/github/cc4h_robotics_ftc/meepmeeptesting/MeepMeepTesting.kt
@@ -18,15 +18,18 @@ object MeepMeepTesting {
 
         myBot.runAction(
             myBot.drive.actionBuilder(Pose2d(-25.0, -62.0, Math.PI / 2))
-                .splineTo(Vector2d(0.0, -34.0), Math.PI / 2)
+                .splineTo(Vector2d(0.0, -34.0), Math.PI / 2) //Go to submersible
                 .waitSeconds(1.0) // TODO: Hang Specimen
-                .splineTo(Vector2d(0.0, -54.0), Math.PI / 2)
-                .splineTo(Vector2d(-48.0, -36.0), Math.PI / 2)
+                .strafeTo(Vector2d(0.0, -40.0)) //Backup as to avoid stuff
+                .strafeTo(Vector2d(-48.0, -40.0))//Go to sample
                 .waitSeconds(1.0) // TODO: Pick Up Sample
-                .splineTo(Vector2d(-52.0, -52.0), Math.PI / 4)
+//                .splineTo(Vector2d(-52.0, -52.0), Math.PI / 4)
+                .strafeTo(Vector2d(-52.0,-52.0))//Go to basket
+                .turn(0 - Math.PI / 4)//Face opposite of basket
                 .waitSeconds(1.0) // TODO: Dump Sample
-                .turnTo(Math.PI / 2)
-                .strafeTo(Vector2d(48.0, -62.0))
+//                .turnTo(Math.PI / 2)
+                .splineTo(Vector2d(54.0, -40.0), Math.PI / 2) //Get ready
+                .strafeTo(Vector2d(54.0, -62.0))
 
 
 //                .lineToX(30.0)
